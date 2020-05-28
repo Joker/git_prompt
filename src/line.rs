45d4553fffaff647bec808de95fa1f1054490cf6
Joker <nice.way@live.com>
2020-05-29 00:32:03 +0300

    new color

--- src/line.rs
+++ src/line.rs
@@ -1,18 +1,19 @@
-const GREEN_L: &str = "\x1b[1;92m";
+const GREEN_L: &str = "\x1b[38;5;28m";
 const BLUE_L: &str = "\x1b[1;94m";
 const YELLOW: &str = "\x1b[0;33m";
-const RESET: &str = "\x1b[0m";
+// const RESET: &str = "\x1b[0m";
 
 pub fn zsh_ps1(git: String) -> String {
 	format!(
-		"{3}┌─{1}%u@%m {2}%~ {0}\n{3}└ >_ {4}",
-		git, GREEN_L, BLUE_L, YELLOW, RESET
+		// "{3}┌─{1}%n@%m {2}%~ {0}{3}\n└ >_ ",
+		"{3}┌─{1}%n {2}%~ {0}{3}\n└ >_ ",
+		git, GREEN_L, BLUE_L, YELLOW
 	)
 }
 
 pub fn bash_ps1(git: String) -> String {
 	format!(
-		"{3}┌─{1}\\u@\\h {2}\\W {0}\n{3}└ >_ {4}",
-		git, GREEN_L, BLUE_L, YELLOW, RESET
+		"{3}┌─{1}\\u@\\h {2}\\W {0}{3}\n└ >_ ",
+		git, GREEN_L, BLUE_L, YELLOW
 	)
 }
