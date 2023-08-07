6436e0d4cf4af5bd2970030606a3d1308726a3c2
Joker <nice.way@live.com>
2023-08-08 00:44:04 +0300

    eval "$(git_prompt zsh init)"

--- src/line.rs
+++ src/line.rs
@@ -1,4 +1,17 @@
-use crate::git::{BLUE_L, GREEN_L, RESET, YELLOW};
+use crate::git::{BLUE_L, CYAN, GREEN_L, RESET, YELLOW};
+
+pub static ZSH: &str = r#"
+ps1func () {
+	PROMPT="$(git_prompt zsh ps1)"
+}
+precmd_functions+=(ps1func)
+"#;
+pub static BASH: &str = r#"
+ps1func () {
+	PS1="$(git_prompt bash ps1)"
+}
+PROMPT_COMMAND=ps1func
+"#;
 
 pub fn zsh_ps1(git: String) -> String {
 	format!(
@@ -9,8 +22,8 @@ pub fn zsh_ps1(git: String) -> String {
 
 pub fn bash_ps1(git: String) -> String {
 	format!(
-		"{0}┌─{1}\\u {2}\\W {3}{0}\n└ >_{4} ", // \\u@\\h
-		YELLOW, GREEN_L, BLUE_L, git, RESET
+		"{0}┌─{1}\\u {2}\\w {3}{0}\n└ >_{4} ", // \\u@\\h
+		CYAN, GREEN_L, BLUE_L, git, RESET
 	)
 	.replace("%{", "")
 	.replace("%}", "")
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-use git::prompt;
-use std::env;
-
 mod git;
 mod line;
 
 fn main() {
-	// let arg = env::args().skip(1).next();
-	let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = std::env::args().collect();
 
 	match args.len() {
-		2 => {
-			let cmd = &args[1];
-			match &cmd[..] {
-				"zsh" => print!("{}", prompt()),
-				_ => print!("{}", prompt().replace("%{", "").replace("%}", "")),
-			}
-		}
-		3 => {
-			let cmd = &args[1];
-			match &cmd[..] {
-				"zsh" => print!("{}", line::zsh_ps1(prompt())),
-				_ => print!("{}", line::bash_ps1(prompt())),
-			}
-		}
-		_ => print!("{}", prompt().replace("%{", "").replace("%}", "")),
+		3 => match (args[1].as_str(), args[2].as_str()) {
+			("zsh", "init") => print!("{}", line::ZSH),
+			("zsh", "ps1") => print!("{}", line::zsh_ps1(git::prompt())),
+			("bash", "init") => print!("{}", line::BASH),
+			_ => print!(
+				"{}",
+				line::bash_ps1(git::prompt().replace("%{", "").replace("%}", ""))
+			),
+		},
+		_ => print!("{}", git::prompt().replace("%{", "").replace("%}", "")),
 	}
 }
